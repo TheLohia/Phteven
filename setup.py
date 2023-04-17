31d29e07ef3d27a1714349447bfd2ad599353367
TheLohia <mananlohia@gmail.com>
2023-04-17 17:11:21 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Developing a model to detect rotting meat in supermarkets',
+    author='Team 9',
+    license='MIT',
+)
